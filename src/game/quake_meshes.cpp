ddf24d00aeccc798de477744444fb4104aaec010
Lucas Alber <lucasd.alber@gmail.com>
2026-04-27 19:01:14 +0200

    wip

--- src/game/quake_meshes.cpp
+++
@@ -1,9 +0,0 @@
-#include "quake_meshes.hpp"
-
-namespace merian_quake {
-
-// All Quake mesh types implement get_data_view() inline; nothing to do here
-// for now. Kept as a translation unit so future helpers can land here without
-// re-plumbing the build system.
-
-} // namespace merian_quake
